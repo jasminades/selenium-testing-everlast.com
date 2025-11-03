8622120d0b4b9f8c1f3176474c316a5b0e17c893
jasminades <jasmina.destanovic1@gmail.com>
2025-11-03 09:23:39 +0100

    registration tests update

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,31 +1,28 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.time.Duration;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class RegistrationTest extends BaseTest {
-    private WebDriverWait wait;
+
+    @BeforeEach
+    public void getUrl(){
+        driver.get("https://www.ekupi.ba/bs/login");
+    }
 
     @Test
     public void testSuccessfulRegistration() {
-        driver.get("https://www.ekupi.ba/bs/login");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         WebElement titleDropdown = wait.until(ExpectedConditions.elementToBeClickable(By.id("register.title")));
         new Select(titleDropdown).selectByValue("g");
         driver.findElement(By.id("register.firstName")).sendKeys("ime");
         driver.findElement(By.id("register.lastName")).sendKeys("prezime");
-        driver.findElement(By.id("register.email")).sendKeys("ime.prezime@gmail.com");
+        driver.findElement(By.id("register.email")).sendKeys("ime.prezime@mail.com");
         driver.findElement(By.id("password")).sendKeys("password123");
         driver.findElement(By.id("register.checkPwd")).sendKeys("password123");
 
@@ -37,8 +34,6 @@ public class RegistrationTest extends BaseTest {
 
     @Test
     void testEmptyForm() {
-        driver.get("https://www.ekupi.ba/bs/login");
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement form = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ekupiRegisterForm")));
 
@@ -69,16 +64,13 @@ public class RegistrationTest extends BaseTest {
     }
 
 
-
-    // radi
     @Test
     public void testMismatchedPasswords() {
-        driver.get("https://www.ekupi.ba/bs/login");
         new Select(driver.findElement(By.id("register.title"))).selectByValue("g");
 
-        driver.findElement(By.id("register.firstName")).sendKeys("TestIme");
-        driver.findElement(By.id("register.lastName")).sendKeys("TestPrezime");
-        driver.findElement(By.id("register.email")).sendKeys("test" + System.currentTimeMillis() + "@example.com");
+        driver.findElement(By.id("register.firstName")).sendKeys("ime");
+        driver.findElement(By.id("register.lastName")).sendKeys("prezime");
+        driver.findElement(By.id("register.email")).sendKeys("ime.prezime@mail.com");
         driver.findElement(By.id("password")).sendKeys("password123");
         driver.findElement(By.id("register.checkPwd")).sendKeys("password1");
 
@@ -102,5 +94,89 @@ public class RegistrationTest extends BaseTest {
         } catch (TimeoutException e) {
             System.out.println("No visible alert");
         }
+    }
+
+    @Test
+    public void testInvalidEmailFormat() {
+        new Select(driver.findElement(By.id("register.title"))).selectByValue("g");
+        driver.findElement(By.id("register.firstName")).sendKeys("ime");
+        driver.findElement(By.id("register.lastName")).sendKeys("prezime");
+        driver.findElement(By.id("register.email")).sendKeys("imeprezime");
+        driver.findElement(By.id("password")).sendKeys("password123");
+        driver.findElement(By.id("register.checkPwd")).sendKeys("password123");
+
+        WebElement termsCheckbox = driver.findElement(By.cssSelector("input[name='termsCheck']"));
+        WebElement gdprCheckbox = driver.findElement(By.cssSelector("input[name='gdprCheck']"));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", termsCheckbox);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", gdprCheckbox);
+
+        WebElement registerButton = driver.findElement(By.id("register-submit-btn"));
+        assertTrue(registerButton.isEnabled());
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", registerButton);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        boolean validationShown = false;
+
+        try {
+            WebElement emailError = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                    By.cssSelector("div.form-group.has-error input#register\\.email, span.help-block, .alert-danger")
+            ));
+            validationShown = emailError.isDisplayed();
+        } catch (TimeoutException e) {
+            validationShown = false;
+        }
+        assertTrue(validationShown);
+    }
+
+
+
+    @Test
+    public void testPasswordTooShort() {
+        new Select(driver.findElement(By.id("register.title"))).selectByValue("g");
+        driver.findElement(By.id("register.firstName")).sendKeys("ime");
+        driver.findElement(By.id("register.lastName")).sendKeys("prezime");
+        driver.findElement(By.id("register.email")).sendKeys("ime.prezime@mail.com");
+        driver.findElement(By.id("password")).sendKeys("123");
+        driver.findElement(By.id("register.checkPwd")).sendKeys("123");
+
+        WebElement helpBlock = driver.findElement(By.id("password_minchar"));
+        assertTrue(helpBlock.isDisplayed());
+        assertTrue(helpBlock.getText().contains("Minimalna dužina"));
+
+        WebElement registerButton = driver.findElement(By.id("register-submit-btn"));
+        assertFalse(registerButton.isEnabled());
+    }
+
+    @Test
+    public void testUncheckedTermsValidation() {
+        new Select(driver.findElement(By.id("register.title"))).selectByValue("g");
+        driver.findElement(By.id("register.firstName")).sendKeys("ime");
+        driver.findElement(By.id("register.lastName")).sendKeys("prezime");
+        driver.findElement(By.id("register.email")).sendKeys("ime.prezime@mail.com");
+        driver.findElement(By.id("password")).sendKeys("password123");
+        driver.findElement(By.id("register.checkPwd")).sendKeys("password123");
+
+        WebElement registerButton = driver.findElement(By.id("register-submit-btn"));
+        assertFalse(registerButton.isEnabled());
+    }
+
+    @Test
+    public void testRegisterButtonEnabledAfterValidInput() {
+        new Select(driver.findElement(By.id("register.title"))).selectByValue("g");
+        driver.findElement(By.id("register.firstName")).sendKeys("ime");
+        driver.findElement(By.id("register.lastName")).sendKeys("prezime");
+        driver.findElement(By.id("register.email")).sendKeys("ime.prezime@mail.com");
+        driver.findElement(By.id("password")).sendKeys("password123");
+        driver.findElement(By.id("register.checkPwd")).sendKeys("password123");
+
+        WebElement termsCheckbox = driver.findElement(By.cssSelector("input[name='termsCheck']"));
+        WebElement gdprCheckbox = driver.findElement(By.cssSelector("input[name='gdprCheck']"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", termsCheckbox);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", gdprCheckbox);
+
+        WebElement registerButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("register-submit-btn")));
+        assertTrue(registerButton.isEnabled());
     }
 }
